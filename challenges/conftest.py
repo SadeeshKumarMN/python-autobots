9c7d8de94eb7692e7c04d0a5892ab8b43bd6da6a
Sadeesh Kumar MN <sadeeshkumarmn@gmail.com>
2020-03-04 17:22:25 +0530

    Updated for Challenge#7
    1. Challenge#7 based on Review Comments;  
    2. Conftest.py for chrome driver path

--- challenges/conftest.py
+++ challenges/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -6,13 +8,14 @@ from selenium.webdriver.support.wait import WebDriverWait
 @pytest.fixture
 def driver():
     # code before yield is run Before Each test
-    driver = webdriver.Chrome('F:\\sadeesh\\code\\python-autobots\\venv\\chromedriver.exe')
+    ROOT_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    driver_path = os.path.join(os.path.sep, ROOT_DIR, 'venv' + os.sep)
+    driver = webdriver.Chrome(driver_path+'chromedriver.exe')
     driver.maximize_window()
     yield driver
     # code after yield is run After Each test
     driver.quit()
 
-
 @pytest.fixture
 def wait(driver):
-    return WebDriverWait(driver, timeout=20)
+    return WebDriverWait(driver, timeout=10)
--- challenges/test_challenge7.py
+++ challenges/test_challenge7.py
@@ -7,27 +7,60 @@ Look at the Makes/Models section of the page
 Create a two-dimensional list that stores the names of the Make/Model as well as their URLs
 Check that each element in this list navigates to the correct page
 """
-
+import sys
+import traceback
+from pprint import pprint
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_verify_each_make_and_model(driver, wait):
     driver.get('https://www.copart.com/')
-    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "li[ng-repeat*='popular']>a")))
-    make_models = driver.find_elements_by_css_selector("li[ng-repeat*='popular']>a")
-    # Created 2D list for store name and URL of each make/model
-    make_models_with_urls = []
-    for make_model in make_models:
-        name = make_model.text
-        url = make_model.get_attribute('href')
-        make_models_with_urls.append([name, url])
+    wait.until(EC.presence_of_all_elements_located((By.XPATH, "//a[starts-with(@href,'./popular')]")))
+    popular_items = driver.find_elements_by_xpath("//a[starts-with(@href,'./popular')]")
 
-    # Verified the navigation of each URL belongs to make/model goes to the correct page
+    # Created 2D list for store name and URL of each popular items
+    popular_vehicles = []
+    for vehicle in popular_items:
+        name = vehicle.text
+        url = vehicle.get_attribute('href')
+        popular_vehicles.append([name, url])
+    print("Count# of vehicles under "'Most Popular items'" section is {}".format(len(popular_vehicles)))
+
+    # Verified the navigation of each URL belongs to popular items(make/models,categories) goes to the correct page
     print()
-    for vehicle in make_models_with_urls:
-        name = vehicle[0]
-        url = vehicle[1]
-        driver.get(url)
-        wait.until(lambda _: (name.replace(" ", "-").lower()) in driver.title)
-        print(f'{vehicle} PASSED')
+    success_items = []
+    failure_items = []
+    error_messages = set([])
+    try:
+        for vehicle in popular_vehicles:
+            name = vehicle[0]
+            url = vehicle[1]
+            driver.get(url)
+            wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//*[@id='searchResultsHeader']")))
+            if name.replace(" ", "-").replace("/", "-").lower() in driver.title:
+                success_items.append([name, url])
+            else:
+                failure_items.append([name, url])
+    except BaseException as error:
+        # This comment block is about to capture the error/exception thrown at specific line
+        # _, _, tb = sys.exc_info()
+        # traceback.print_tb(tb)
+        # tb_info = traceback.extract_tb(tb)
+        # filename, line, func, text = tb_info[-1]
+        # error_messages.add('An error occurred on line {} in statement {}'.format(line, text))
+        error_messages.add(error)
+    finally:
+        if len(success_items) == len(popular_vehicles):
+            print("Success! All the vehicles belongs to popular section are navigated to the correct page")
+            print()
+            pprint(success_items)
+        elif len(failure_items) > 0:
+            print("Count# of incorrect-navigation items: {}".format(len(failure_items)))
+            pprint(failure_items)
+            print()
+            print("Count# of correct-navigation items: {}".format(len(success_items)))
+            pprint(success_items)
+        else:
+            print("Exceptions/Errors#:")
+            pprint(error_messages)
